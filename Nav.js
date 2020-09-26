a3259e7bb13f96657c8340a4314a839d4ea23714
unknown <kanikamittal63@gmail.com>
2020-09-26 23:06:47 +0530

    netflix-clone with reactjs

---
+++ Nav.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://media.idownloadblog.com/wp-content/uploads/2018/01/Netflix-Logo.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB0OsgN2PBtMn_7L2czeWwvbg7muOprLJQ",
+  authDomain: "netflix-clone-e9533.firebaseapp.com",
+  databaseURL: "https://netflix-clone-e9533.firebaseio.com",
+  projectId: "netflix-clone-e9533",
+  storageBucket: "netflix-clone-e9533.appspot.com",
+  messagingSenderId: "590376010422",
+  appId: "1:590376010422:web:3474b97a57c07138ed5c17",
+  measurementId: "G-QVH77HTLDV",
+};
